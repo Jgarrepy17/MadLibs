a3b3e62026445cb7dc8cd2821958248bcb6e46cb
Jgarrepy17 <jgarrepy@pathfinderstudents.org>
2017-10-02 13:55:23 -0400

    Add files via upload

--- Madlibs.js
+++ Madlibs.js
@@ -1,27 +1,31 @@
-//Mad Libs
-// Jake Garrepy
-
-//Initilize variables
+// initialize variables
 var sentence=[];
 var replacements=[];
 var index=0;
-
-//Initilize Users
+// initialize users
 alert ("Filler, Go Away.");
-alert ("End with shift+6(^)");
-
-//Fill sentence
-populate(index, sentence, "word");
-alert(sentence)
-
-index = 0;
-//Specify Position
+alert ("Enter after each word, end with Shift+6(^)");
+// fill sentence
+populate(index,sentence,"word");
+alert (sentence);
+index=0;
+// specify positions
+populate(index,replacements,"position");
 alert (replacements);
-populate(index, replacements, "position");
-replace(index,sentence,replacements,"part of speech")
-alert(sentence)
-function populate(sindex, sarray, label){
-	do{
+index=0;
+replace(index,sentence,replacements,"part of speech");
+alert (sentence);
+// switch people
+// train filler
+alert("Enter words that match the part of speech shown");
+index=0;
+replace(index, sentence, replacements, "new word");
+
+alert('Go get the author to see the sentence')
+sentenceString = sentence.join(' ')
+alert(sentenceString.toString())
+function populate(sindex,sarray,label){
+	do {
 		sarray[sindex]=prompt("Next "+label+"?");
 		sindex = sindex+1;
 	}
@@ -31,9 +35,14 @@ function populate(sindex, sarray, label){
 	return sarray;
 }
 
-function replace(sindex,sarray,rarray,label){
-	for (words = 0; words < rarray.length; words++){
-		sarray[rarray[words]] = prompt("Next" +label+"?")
+function replace (sindex,sarray,rarray,label) {
+	for (sindex = 0; sindex < rarray.length; sindex++) {
+		if (label == 'new word'){
+			sarray[rarray[sindex]] = prompt("provide a "+sarray[rarray[sindex]]+" here");
+		}
+		else{
+		sarray[rarray[sindex]] = prompt("next "+label+"?");
+		}
 	}
-	return(sarray)
+	return sarray;
 }
